b3dcb47e4f25debba8520dc2ed0fa1465e50afbb
PriyaBalaDharshini <125110897+PriyaBalaDharshini@users.noreply.github.com>
2023-05-08 15:21:07 +0530

    Update script3.js

--- script3.js
+++ script3.js
@@ -19,7 +19,11 @@ request.open("GET","https://restcountries.com/v3.1/all");
 request.send();
 request.onload=function(){
 var result=JSON.parse(request.response);
-console.log(result.flag);
+console.log(result);
+for(var i=0; i<result.length; i++){
+  console.log(result[i].flag);
+}
+}
 
 //Question 3
 var request=new XMLHttpRequest();
